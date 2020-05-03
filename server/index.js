00ae62aca48434f107d30ac111b808d951c6bc40
nap3831 <nap3831@gmail.com>
2020-05-03 23:09:53 +0700

    template express backend

--- server/index.js
+++ server/index.js
@@ -5,11 +5,12 @@ const app = express();
 const port = process.env.process || 5000;
 const products = require('./routes/api/products');
 const users = require('./routes/api/users');
-
+// Parse body text to json 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
+// router import form route
 app.use('/routes/api/products', products);
 app.use('/routes/api/users', users);
 
